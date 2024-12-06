4b9807add78d6b53ad6f9b00019f611c9e4406c4
Leonardo <leo.guzman.cruz04@gmail.com>
2024-12-06 02:33:58 -0500

    Seccion 5: finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModules } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModules
+    HeroesModules,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
